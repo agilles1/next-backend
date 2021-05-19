eb08ee09cf9b09ac94fbf6d8c9f36118e6602710
agilles1 <adam.g.tpt@gmail.com>
2021-05-18 22:59:33 -0400

    Generate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Audition.destroy_all
+Audition.reset_pk_sequence
+
+Candidate.destroy_all
+Candidate.reset_pk_sequence
+
+Room.destroy_all
+Room.reset_pk_sequence
+
+
+a = Audition.create(date: DateTime.now, instrument: "violin" )
+
+20.times { 
+    f = Faker::Name.first_name
+    l = Faker::Name.last_name
+    full_name = f + " " + l
+    p = Candidate.new(name: full_name, email: Faker::Internet.email(name: f), phone: Faker::PhoneNumber.phone_number)
+    p.save
+    p.audition = a
+    p.save
+}
+
+rooms = ["Green Room", "255","256", "RR1"]
+
+rooms.each_with_index do |r, i|
+    room = Room.new(name: r, order: i)
+    room.save
+    room.audition = a
+    room.save
+end
