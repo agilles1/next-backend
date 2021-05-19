7d01a5a28431a4f7881250282fca9d850ed5c501
agilles1 <adam.g.tpt@gmail.com>
2021-05-19 17:23:00 -0400

    create methods for flow of room

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,12 +2,24 @@ class Candidate < ApplicationRecord
     has_one :candidate_room
     has_one :room, through: :candidate_room
     belongs_to :audition
-    
-    def on_deck()
 
+    def assign_room
+        self.room = Room.next_avail_room
+        self.save
     end
 
-    def assign_room()
+    def on_deck
+        self.room = Room.find_by(name: "Green Room")
+        self.save
+    end
 
+    def to_stage
+        self.room = Room.find_by(name: "Stage")
+        self.save
+    end
+
+    def clear_room
+        self.room = nil
+        self.save
     end
 end
