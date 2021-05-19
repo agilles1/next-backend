f3747aa4ca50c325f4de965c512ddb9324ce702d
agilles1 <adam.g.tpt@gmail.com>
2021-05-19 17:14:59 -0400

    Update next_avail_room to omit GR and Stage

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,16 +2,15 @@ class Room < ApplicationRecord
     has_one :candidate_room
     has_one :candidate, through: :candidate_room
     belongs_to :audition
+    validates :fill_order, uniqueness: true 
     
-
-    def self.next_avail_room
+        def self.next_avail_room
         available_rooms = []
-        rooms = self.all.order(:order)
-
+        rooms = self.where('fill_order >= 2').order(:fill_order)
         rooms.each do |room|
             !room.candidate ? available_rooms.push(room) : nil
         end
-        
+
         return available_rooms.first
     end
     
