f6127cecb62655cee2acab3e5b09adf2a8f9eb9b
agilles1 <adam.g.tpt@gmail.com>
2021-05-19 22:07:04 -0400

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :candidate_rooms
-  resources :rooms
-  resources :candidates
-  resources :auditions
+  # resources :rooms
+  namespace :api do
+    namespace :v1 do
+      resources :candidates
+
+      resources :auditions do
+        :candidates
+      end
+
+    end
+  end
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
