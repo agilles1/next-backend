93333e2ef2c0a582fb7db5c15db911fa5f7771cb
agilles1 <adam.g.tpt@gmail.com>
2021-05-20 18:48:59 -0400

    add candidate routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      resources :candidates
+      
       resources :auditions do
         resources :candidates
       end
