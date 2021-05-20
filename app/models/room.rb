8478d8812ed47326f9ae148d4d871e729842fe67
agilles1 <adam.g.tpt@gmail.com>
2021-05-19 21:30:55 -0400

    Adds scope method to filter group v single

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,17 +1,24 @@
 class Room < ApplicationRecord
-    has_one :candidate_room
-    has_one :candidate, through: :candidate_room
+    has_many :candidate_rooms
+    has_many :candidates, through: :candidate_rooms
     belongs_to :audition
     validates :fill_order, uniqueness: true 
+    scope :single_rooms, -> {where('holding == false').where('fill_order > 2').order(:fill_order)}
+    scope :group_rooms, -> {where('holding == true').order(:fill_order)}
+
     
         def self.next_avail_room
         available_rooms = []
-        rooms = self.where('fill_order >= 2').order(:fill_order)
+
+        rooms = self.single_rooms
+    
         rooms.each do |room|
-            !room.candidate ? available_rooms.push(room) : nil
+            room.candidates.length < 1 ? available_rooms.push(room) : nil
+        end
+                   
+        available_rooms.first
         end
 
-        return available_rooms.first
-    end
+
     
 end
