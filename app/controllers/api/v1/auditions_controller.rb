d841bce8bfeec94b9679f0513e465a0fb5b905f6
agilles1 <adam.g.tpt@gmail.com>
2021-05-19 22:30:16 -0400

    Stub out audition_params

--- app/controllers/api/v1/auditions_controller.rb
+++ app/controllers/api/v1/auditions_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::AuditionsController < ApplicationController
-    before
+    before_action :set_audition
 
     def show
-
+      render json: @audition
     end
 
     private
 
     def audition_params
-        
+        params.require(:audition).permit(:date, :instrument)
+    end
+
+    def set_audition
+        @audition = Audition.find(params[:id])
     end
 end
