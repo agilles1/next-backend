e81625be417bc8d29b03ee3f9f42946dbb6f45a5
agilles1 <adam.g.tpt@gmail.com>
2021-05-21 23:23:33 -0400

    Start new_with_rooms method

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -6,5 +6,10 @@ class Audition < ApplicationRecord
         self.counter += 1
         self.save
     end
-    
+
+    def self.new_with_rooms(params)
+        datetime = 
+        a = Audition.new(params["date"])
+    end
+    # "date"=>"11:00", "instrument"=>"test", "holding"=>true, "rooms"=>"255, 256, 257, 258"
 end
